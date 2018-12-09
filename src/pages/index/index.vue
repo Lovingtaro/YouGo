eac9049cdf64c2546ec08a28dcb51a78c8777cc2
Lovingtaro <732704471@qq.com>
2018-12-09 15:33:22 +0800

    初始化微信小程序项目yougo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li  @longtap="delOne(index)"  v-for="(item, index) in messages" :key="index">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data:function () {
+    return {
+      messages: ['你好吗？','不好'] 
+    }
+  },
+ 
+  methods: {
+    delOne(index){
+      wx.showModal({
+        title:"警告",
+        content:"你确定删除吗？"
+      })
+
+      }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
